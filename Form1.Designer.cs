9351a195338fdd439db3558dc7aae22a96dca57a
劲松 冯 <503868944@qq.com>
2020-05-15 11:01:09 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Calculator_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Text_Lab = new System.Windows.Forms.Label();
+            this.Value_Lab = new System.Windows.Forms.Label();
+            this.Btn_8 = new System.Windows.Forms.Button();
+            this.Btn_9 = new System.Windows.Forms.Button();
+            this.Btn_7 = new System.Windows.Forms.Button();
+            this.Btn_4 = new System.Windows.Forms.Button();
+            this.Btn_6 = new System.Windows.Forms.Button();
+            this.Btn_5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.Btn_3 = new System.Windows.Forms.Button();
+            this.Btn_2 = new System.Windows.Forms.Button();
+            this.Btn_dot = new System.Windows.Forms.Button();
+            this.Btn_back = new System.Windows.Forms.Button();
+            this.Btn_0 = new System.Windows.Forms.Button();
+            this.Btn_equal = new System.Windows.Forms.Button();
+            this.Btn_add = new System.Windows.Forms.Button();
+            this.Btn_minus = new System.Windows.Forms.Button();
+            this.Btn_multiply = new System.Windows.Forms.Button();
+            this.Btn_divide = new System.Windows.Forms.Button();
+            this.Btn_F = new System.Windows.Forms.Button();
+            this.Btn_R = new System.Windows.Forms.Button();
+            this.Btn_clean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Text_Lab
+            // 
+            this.Text_Lab.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Text_Lab.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Text_Lab.Location = new System.Drawing.Point(33, 36);
+            this.Text_Lab.Name = "Text_Lab";
+            this.Text_Lab.Size = new System.Drawing.Size(443, 73);
+            this.Text_Lab.TabIndex = 0;
+            this.Text_Lab.Text = "0";
+            // 
+            // Value_Lab
+            // 
+            this.Value_Lab.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Value_Lab.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Value_Lab.ForeColor = System.Drawing.Color.DarkRed;
+            this.Value_Lab.Location = new System.Drawing.Point(39, 71);
+            this.Value_Lab.Name = "Value_Lab";
+            this.Value_Lab.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Value_Lab.Size = new System.Drawing.Size(437, 38);
+            this.Value_Lab.TabIndex = 1;
+            this.Value_Lab.Text = "0";
+            // 
+            // Btn_8
+            // 
+            this.Btn_8.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_8.Location = new System.Drawing.Point(139, 164);
+            this.Btn_8.Name = "Btn_8";
+            this.Btn_8.Size = new System.Drawing.Size(86, 89);
+            this.Btn_8.TabIndex = 2;
+            this.Btn_8.Text = "8";
+            this.Btn_8.UseVisualStyleBackColor = true;
+            // 
+            // Btn_9
+            // 
+            this.Btn_9.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_9.Location = new System.Drawing.Point(231, 164);
+            this.Btn_9.Name = "Btn_9";
+            this.Btn_9.Size = new System.Drawing.Size(86, 89);
+            this.Btn_9.TabIndex = 3;
+            this.Btn_9.Text = "9";
+            this.Btn_9.UseVisualStyleBackColor = true;
+            // 
+            // Btn_7
+            // 
+            this.Btn_7.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_7.Location = new System.Drawing.Point(47, 164);
+            this.Btn_7.Name = "Btn_7";
+            this.Btn_7.Size = new System.Drawing.Size(86, 89);
+            this.Btn_7.TabIndex = 4;
+            this.Btn_7.Text = "7";
+            this.Btn_7.UseVisualStyleBackColor = true;
+            // 
+            // Btn_4
+            // 
+            this.Btn_4.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_4.Location = new System.Drawing.Point(47, 259);
+            this.Btn_4.Name = "Btn_4";
+            this.Btn_4.Size = new System.Drawing.Size(86, 89);
+            this.Btn_4.TabIndex = 7;
+            this.Btn_4.Text = "4";
+            this.Btn_4.UseVisualStyleBackColor = true;
+            // 
+            // Btn_6
+            // 
+            this.Btn_6.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_6.Location = new System.Drawing.Point(231, 259);
+            this.Btn_6.Name = "Btn_6";
+            this.Btn_6.Size = new System.Drawing.Size(86, 89);
+            this.Btn_6.TabIndex = 6;
+            this.Btn_6.Text = "6";
+            this.Btn_6.UseVisualStyleBackColor = true;
+            // 
+            // Btn_5
+            // 
+            this.Btn_5.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_5.Location = new System.Drawing.Point(139, 259);
+            this.Btn_5.Name = "Btn_5";
+            this.Btn_5.Size = new System.Drawing.Size(86, 89);
+            this.Btn_5.TabIndex = 5;
+            this.Btn_5.Text = "5";
+            this.Btn_5.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.Location = new System.Drawing.Point(47, 354);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(86, 89);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "1";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Btn_3
+            // 
+            this.Btn_3.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_3.Location = new System.Drawing.Point(231, 354);
+            this.Btn_3.Name = "Btn_3";
+            this.Btn_3.Size = new System.Drawing.Size(86, 89);
+            this.Btn_3.TabIndex = 9;
+            this.Btn_3.Text = "3";
+            this.Btn_3.UseVisualStyleBackColor = true;
+            // 
+            // Btn_2
+            // 
+            this.Btn_2.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_2.Location = new System.Drawing.Point(139, 354);
+            this.Btn_2.Name = "Btn_2";
+            this.Btn_2.Size = new System.Drawing.Size(86, 89);
+            this.Btn_2.TabIndex = 8;
+            this.Btn_2.Text = "2";
+            this.Btn_2.UseVisualStyleBackColor = true;
+            // 
+            // Btn_dot
+            // 
+            this.Btn_dot.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_dot.Location = new System.Drawing.Point(47, 449);
+            this.Btn_dot.Name = "Btn_dot";
+            this.Btn_dot.Size = new System.Drawing.Size(86, 89);
+            this.Btn_dot.TabIndex = 13;
+            this.Btn_dot.Text = ".";
+            this.Btn_dot.UseVisualStyleBackColor = true;
+            // 
+            // Btn_back
+            // 
+            this.Btn_back.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_back.Location = new System.Drawing.Point(231, 449);
+            this.Btn_back.Name = "Btn_back";
+            this.Btn_back.Size = new System.Drawing.Size(86, 89);
+            this.Btn_back.TabIndex = 12;
+            this.Btn_back.Text = "<";
+            this.Btn_back.UseVisualStyleBackColor = true;
+            // 
+            // Btn_0
+            // 
+            this.Btn_0.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_0.Location = new System.Drawing.Point(139, 449);
+            this.Btn_0.Name = "Btn_0";
+            this.Btn_0.Size = new System.Drawing.Size(86, 89);
+            this.Btn_0.TabIndex = 11;
+            this.Btn_0.Text = "0";
+            this.Btn_0.UseVisualStyleBackColor = true;
+            // 
+            // Btn_equal
+            // 
+            this.Btn_equal.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_equal.Location = new System.Drawing.Point(400, 390);
+            this.Btn_equal.Name = "Btn_equal";
+            this.Btn_equal.Size = new System.Drawing.Size(69, 148);
+            this.Btn_equal.TabIndex = 14;
+            this.Btn_equal.Text = "=";
+            this.Btn_equal.UseVisualStyleBackColor = true;
+            // 
+            // Btn_add
+            // 
+            this.Btn_add.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_add.Location = new System.Drawing.Point(323, 467);
+            this.Btn_add.Name = "Btn_add";
+            this.Btn_add.Size = new System.Drawing.Size(71, 71);
+            this.Btn_add.TabIndex = 15;
+            this.Btn_add.Text = "+";
+            this.Btn_add.UseVisualStyleBackColor = true;
+            // 
+            // Btn_minus
+            // 
+            this.Btn_minus.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_minus.Location = new System.Drawing.Point(323, 390);
+            this.Btn_minus.Name = "Btn_minus";
+            this.Btn_minus.Size = new System.Drawing.Size(71, 71);
+            this.Btn_minus.TabIndex = 16;
+            this.Btn_minus.Text = "-";
+            this.Btn_minus.UseVisualStyleBackColor = true;
+            this.Btn_minus.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Btn_multiply
+            // 
+            this.Btn_multiply.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_multiply.Location = new System.Drawing.Point(323, 313);
+            this.Btn_multiply.Name = "Btn_multiply";
+            this.Btn_multiply.Size = new System.Drawing.Size(71, 71);
+            this.Btn_multiply.TabIndex = 17;
+            this.Btn_multiply.Text = "x";
+            this.Btn_multiply.UseVisualStyleBackColor = true;
+            // 
+            // Btn_divide
+            // 
+            this.Btn_divide.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_divide.Location = new System.Drawing.Point(323, 235);
+            this.Btn_divide.Name = "Btn_divide";
+            this.Btn_divide.Size = new System.Drawing.Size(71, 71);
+            this.Btn_divide.TabIndex = 18;
+            this.Btn_divide.Text = "/";
+            this.Btn_divide.UseVisualStyleBackColor = true;
+            // 
+            // Btn_F
+            // 
+            this.Btn_F.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_F.Location = new System.Drawing.Point(323, 164);
+            this.Btn_F.Name = "Btn_F";
+            this.Btn_F.Size = new System.Drawing.Size(71, 65);
+            this.Btn_F.TabIndex = 19;
+            this.Btn_F.Text = "(";
+            this.Btn_F.UseVisualStyleBackColor = true;
+            // 
+            // Btn_R
+            // 
+            this.Btn_R.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_R.Location = new System.Drawing.Point(398, 164);
+            this.Btn_R.Name = "Btn_R";
+            this.Btn_R.Size = new System.Drawing.Size(71, 65);
+            this.Btn_R.TabIndex = 20;
+            this.Btn_R.Text = ")";
+            this.Btn_R.UseVisualStyleBackColor = true;
+            // 
+            // Btn_clean
+            // 
+            this.Btn_clean.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_clean.Location = new System.Drawing.Point(398, 235);
+            this.Btn_clean.Name = "Btn_clean";
+            this.Btn_clean.Size = new System.Drawing.Size(71, 149);
+            this.Btn_clean.TabIndex = 21;
+            this.Btn_clean.Text = "C";
+            this.Btn_clean.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(516, 607);
+            this.Controls.Add(this.Btn_clean);
+            this.Controls.Add(this.Btn_R);
+            this.Controls.Add(this.Btn_F);
+            this.Controls.Add(this.Btn_divide);
+            this.Controls.Add(this.Btn_multiply);
+            this.Controls.Add(this.Btn_minus);
+            this.Controls.Add(this.Btn_add);
+            this.Controls.Add(this.Btn_equal);
+            this.Controls.Add(this.Btn_dot);
+            this.Controls.Add(this.Btn_back);
+            this.Controls.Add(this.Btn_0);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.Btn_3);
+            this.Controls.Add(this.Btn_2);
+            this.Controls.Add(this.Btn_4);
+            this.Controls.Add(this.Btn_6);
+            this.Controls.Add(this.Btn_5);
+            this.Controls.Add(this.Btn_7);
+            this.Controls.Add(this.Btn_9);
+            this.Controls.Add(this.Btn_8);
+            this.Controls.Add(this.Value_Lab);
+            this.Controls.Add(this.Text_Lab);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FJSCalculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Text_Lab;
+        private System.Windows.Forms.Label Value_Lab;
+        private System.Windows.Forms.Button Btn_8;
+        private System.Windows.Forms.Button Btn_9;
+        private System.Windows.Forms.Button Btn_7;
+        private System.Windows.Forms.Button Btn_4;
+        private System.Windows.Forms.Button Btn_6;
+        private System.Windows.Forms.Button Btn_5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button Btn_3;
+        private System.Windows.Forms.Button Btn_2;
+        private System.Windows.Forms.Button Btn_dot;
+        private System.Windows.Forms.Button Btn_back;
+        private System.Windows.Forms.Button Btn_0;
+        private System.Windows.Forms.Button Btn_equal;
+        private System.Windows.Forms.Button Btn_add;
+        private System.Windows.Forms.Button Btn_minus;
+        private System.Windows.Forms.Button Btn_multiply;
+        private System.Windows.Forms.Button Btn_divide;
+        private System.Windows.Forms.Button Btn_F;
+        private System.Windows.Forms.Button Btn_R;
+        private System.Windows.Forms.Button Btn_clean;
+    }
+}
+
